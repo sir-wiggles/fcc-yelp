7e877fe41e2fc81904b49983ffd59f651d3307bb
jeff <jeff>
2017-02-02 19:30:03 -0800

    added keys

---
+++ keys.js
@@ -0,0 +1,12 @@
+
+exports.keys = {
+    consumer: {
+        key    : "m_fQlafv8BZCzYCjUuoRAg",
+        secret : "GXEwTxJ0b-fNN0WiJ6OWDnVNANU",
+    },
+    token: {
+        key    : "wl5QRmq7jsjHN0Nt3tEtaYQQNTfaqgXh",
+        secret : "2nh31ytgOLsGq0P9OPWtbMmKIXc"
+    }
+};
+
