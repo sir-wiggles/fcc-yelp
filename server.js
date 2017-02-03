3089c35e8d24500d98a9910047cfde6e3e70fe30
jeff <jeff>
2017-02-02 19:41:22 -0800

    abs paths

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const helpers = require('./helpers');
+const path    = require("path");
 
 const app = express();
-app.use(express.static("client/build"))
+app.use(express.static(path.join(__dirname, "client/build")));
 
 app.get("/", function(req, res) {
-    res.sendFile("client/build/index.html");
+    res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
 
 app.get("/search/:id", function(req, res) {
